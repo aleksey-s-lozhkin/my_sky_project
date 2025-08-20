0b473d0691e0470af21bab39a4d5a132320645c8
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2025-08-20 22:30:44 +0300

    Added test_widget.py, fixed widget.py, test_mask.py

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 from src.processing import filter_by_state, sort_by_date
-from src.widget import get_date, mask_account_card
-
-# Тестовый ввод данных для проверки функций get_date, mask_account_card
-print(mask_account_card('Maestro 1596837868705199'))
-print(mask_account_card('Счет 64686473678894779589'))
-print(mask_account_card('MasterCard 7158300734726758'))
-print(mask_account_card('Счет 35383033474447895560'))
-print(mask_account_card('Visa Classic 6831982476737658'))
-print(mask_account_card('Visa Platinum 8990922113665229'))
-print(mask_account_card('Visa Gold 5999414228426353'))
-print(mask_account_card('Счет 73654108430135874305'))
-print(get_date("2025-07-28T19:06:26.671407"))
 
 # Тестовый ввод для проверки функции filter_by_state
 sample_date = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_card_empty():
@@ -11,18 +11,24 @@ def test_account_empty():
     assert get_mask_account('') == ''
 
 
-@pytest.mark.parametrize("card_number, expected",[('1596837868705199', '1596 83** **** 5199'),
-                                                  ('7158300734726758', '7158 30** **** 6758'),
-                                                  ('6831982476737658', '6831 98** **** 7658'),
-                                                  ('8990922113665229', '8990 92** **** 5229'),
-                                                  ('5999414228426353', '5999 41** **** 6353')])
+@pytest.mark.parametrize(
+    "card_number, expected",
+    [
+        ('1596837868705199', '1596 83** **** 5199'),
+        ('7158300734726758', '7158 30** **** 6758'),
+        ('6831982476737658', '6831 98** **** 7658'),
+        ('8990922113665229', '8990 92** **** 5229'),
+        ('5999414228426353', '5999 41** **** 6353'),
+    ],
+)
 def test_card_valid(card_number, expected):
     assert get_mask_card_number(card_number) == expected
 
 
-@pytest.mark.parametrize('account_number, expected',[('64686473678894779589', '**9589'),
-                                                     ('35383033474447895560', '**5560'),
-                                                     ('73654108430135874305', '**4305')])
+@pytest.mark.parametrize(
+    'account_number, expected',
+    [('64686473678894779589', '**9589'), ('35383033474447895560', '**5560'), ('73654108430135874305', '**4305')],
+)
 def test_account_valid(account_number, expected):
     assert get_mask_account(account_number) == expected
 
