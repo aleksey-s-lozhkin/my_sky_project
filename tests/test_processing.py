3132b823f7074347e2a114096fbc4aa86ffe9039
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2025-08-20 23:58:28 +0300

    Added test_processing.py, conftest.py, fixed processing.py, main.py

---
+++ tests/test_processing.py
@@ -0,0 +1,41 @@
+from src.processing import filter_by_state, sort_by_date
+
+
+def test_filter_sample1(sample):
+    assert filter_by_state(sample) == [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    ]
+
+
+def test_filter_sample2(sample):
+    assert filter_by_state(sample, 'CANCELED') == [
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+    ]
+
+
+def test_sort_sample1(sample):
+    assert sort_by_date(sample) == [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    ]
+
+
+def test_sort_sample2(sample):
+    assert sort_by_date(sample, False) == [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    ]
+
+
+def test_filter_wrong(incorrect_sample):
+    assert filter_by_state(incorrect_sample) == []
+
+
+def test_sort_wrong(incorrect_sample):
+    assert sort_by_date(incorrect_sample) == []
