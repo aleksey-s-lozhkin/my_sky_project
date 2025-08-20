6ecc7083e2c787f6d48037884beda4895886c8d7
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2025-08-20 13:47:32 +0300

    Corrected the file README.md

--- src/processing.py
+++ src/processing.py
@@ -12,7 +12,7 @@ def filter_by_state(request: List[Dict[str, Any]], state: str = 'EXECUTED') -> L
 
 def sort_by_date(sorting_data: List[Dict[str, Any]], sorting_direction: bool = True) -> List[Dict[str, Any]]:
     """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию —
-    убывание). Функция должна возвращать новый список, отсортированный по дате (date)"""
+    убывание). Функция возвращать новый список, отсортированный по дате (date)"""
 
     sorted_by_date = sorted(sorting_data, key=lambda x: x['date'], reverse=sorting_direction)
 
