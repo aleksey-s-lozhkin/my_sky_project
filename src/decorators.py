847e2b4b7b7d66f3ff0a3b171cfa2e87a71bcc20
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2025-08-25 15:26:35 +0300

    Fixed decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -7,6 +7,14 @@ P = ParamSpec('P')
 T = TypeVar('T')
 
 
+def write_log(log_msg: str, filename: str) -> None:
+    if filename:
+        with open(filename, 'a', encoding='utf-8') as log_file:
+            log_file.write(log_msg)
+    else:
+        print(log_msg)
+
+
 def log(filename: str = "mylog.txt") -> Callable[[Callable[P, T]], Callable[P, T]]:
     """Декоратор, который автоматически логирует начало и конец выполнения функции, а также ее результаты или возникшие
     ошибки. Декоратор принимает необязательный аргумент filename, который определяет, куда будут записываться логи (в
@@ -16,34 +24,27 @@ def log(filename: str = "mylog.txt") -> Callable[[Callable[P, T]], Callable[P, T
     def wrapper(func: Callable[P, T]) -> Callable[P, T]:
         @wraps(func)
         def inner(*args: P.args, **kwargs: P.kwargs) -> T:
-            log_msg = ''
+            log_msg = '\n'
             start_func = datetime.now()
-            start_msg = f'{start_func} start "{func.__name__}"\n'
-            log_msg += start_msg
 
             try:
                 result = func(*args, **kwargs)
                 end_func = datetime.now()
+                start_msg = f'{start_func} start "{func.__name__}"\n'
                 end_msg = f'{end_func} stop "{func.__name__}"\n'
-                log_msg += end_msg
                 func_result = f'"{func.__name__}" ok\n'
-                log_msg += func_result
+                log_msg += start_msg + end_msg + func_result
+                write_log(log_msg, filename)
+                return result
 
             except Exception as err:
                 end_func = datetime.now()
+                start_msg = f'{start_func} start "{func.__name__}"\n'
                 end_msg = f'{end_func} stop "{func.__name__}"\n'
-                log_msg += end_msg
                 func_result = f'"{func.__name__}" error: {err}. Inputs: {args}, {kwargs}\n'
-                log_msg += func_result
-
-            finally:
-                if filename:
-                    with open(filename, 'a') as log_file:
-                        log_file.write(log_msg)
-                else:
-                    print(log_msg)
-
-            return result
+                log_msg += start_msg + end_msg + func_result
+                write_log(log_msg, filename)
+                raise
 
         return inner
 
