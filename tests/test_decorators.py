d1e8cd11812c62ecee915b6d0be0945e61bb6f72
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2025-09-06 08:16:01 +0300

    Added tests for amount_transaction Fixed test_decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -2,8 +2,7 @@ import os.path
 
 import pytest
 
-from src.decorators import log, write_log
-from src.utils import check_filename
+from src.decorators import log, check_filename
 
 
 @log(filename='')
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 import os
+import pytest
 
-from unittest.mock import Mock
+from unittest.mock import patch
 
 from src.utils import amount_transactions, open_json_transactions
 
@@ -49,4 +50,81 @@ def test_usd_transaction():
         "from": "Maestro 1596837868705199",
         "to": "Счет 64686473678894779589",
     }
-    assert amount_transactions()
+    with patch('src.utils.currency_convert') as mock_currency_convert:
+        mock_currency_convert.return_value = 30000.0
+        res = amount_transactions(test_dict, 10000)
+        assert res == 40000
+        mock_currency_convert.assert_called_once_with('USD', '20000')
+
+
+def test_eur_transaction():
+    test_dict = {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {"amount": "20000", "currency": {"name": "руб.", "code": "EUR"}},
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
+    with patch('src.utils.currency_convert') as mock_currency_convert:
+        mock_currency_convert.return_value = 30000.0
+        res = amount_transactions(test_dict, 10000)
+        assert res == 40000
+        mock_currency_convert.assert_called_once_with('EUR', '20000')
+
+
+def test_wrong_json():
+    wrong_names = [
+        (
+            {
+                "id": 441945886,
+                "state": "EXECUTED",
+                "date": "2019-08-26T10:50:58.294041",
+                "operationAmount": {"currency": {"name": "руб.", "code": "EUR"}},
+                "description": "Перевод организации",
+                "from": "Maestro 1596837868705199",
+                "to": "Счет 64686473678894779589",
+            },
+            'Key "amount" is missing',
+        ),
+        (
+            {
+                "id": 441945886,
+                "state": "EXECUTED",
+                "date": "2019-08-26T10:50:58.294041",
+                "operationAmount": {"amount": "20000", "currency": {"name": "руб."}},
+                "description": "Перевод организации",
+                "from": "Maestro 1596837868705199",
+                "to": "Счет 64686473678894779589",
+            },
+            'Key "code" is missing',
+        ),
+        (
+            {
+                "id": 441945886,
+                "state": "EXECUTED",
+                "date": "2019-08-26T10:50:58.294041",
+                "operationAmount": {"amount": "20000"},
+                "description": "Перевод организации",
+                "from": "Maestro 1596837868705199",
+                "to": "Счет 64686473678894779589",
+            },
+            'Key "currency" is missing',
+        ),
+        (
+            {
+                "id": 441945886,
+                "state": "EXECUTED",
+                "date": "2019-08-26T10:50:58.294041",
+                "description": "Перевод организации",
+                "from": "Maestro 1596837868705199",
+                "to": "Счет 64686473678894779589",
+            },
+            'Key "operationAmount" is missing',
+        ),
+    ]
+
+    for wrong_dict, expected_error in wrong_names:
+        with pytest.raises(KeyError, match=expected_error):
+            amount_transactions(wrong_dict, '10000')
