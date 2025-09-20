8ee56e211a4071d89994ed40e44be453ce9d9b98
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2025-09-20 19:56:22 +0300

    Added: functions process_bank_search, process_bank_classification in transaction_analyzer.py

--- src/transaction_analyzer.py
+++ src/transaction_analyzer.py
@@ -1,18 +1,34 @@
+import re
 from typing import Any, Dict, List
+from collections import Counter
 
 
-def process_bank_search(data: List[Dict[str, Any]], search: str)->List[Dict[Any, Any]]:
+def process_bank_search(data: List[Dict[str, Any]], search: str)->List[Dict[str, Any]]:
+    """Функция для поиска в списке словарей операций по заданной строке возвращает список словарей с операциями, у
+    которых в описании есть строка, переданная аргументу функции"""
 
-    if not data or not search:
-        raise ValueError('Required parameters data or search are empty')
+    if not data:
+        raise ValueError('Data cannot be empty')
+    if not search:
+        raise ValueError('Search query cannot be empty')
 
-    count = 0
-    for item in data:
-        if item.get('description'):
-            count += 1
+    pattern = re.compile(search, re.IGNORECASE)
+    sorted_by_description = [item for item in data if pattern.search(item.get('description', ''))]
 
-    if count == len(data):
-        sorted_by_description = [item for item in data if item.get('description') == search]
-        return sorted_by_description
-    else:
-        raise Exception('No data found')
+    return sorted_by_description
+
+
+def process_bank_classification(data: List[Dict[str, Any]], classification: List[str])->Dict[str, Any]:
+    """Функция для подсчета количества банковских операций определенного типа возвращает словарь, в котором ключи —
+    это названия категорий, а значения — это количество операций в каждой категории"""
+
+    if not data:
+        raise ValueError('Data cannot be empty')
+    if not classification:
+        raise ValueError('Search query cannot be empty')
+
+    sorted_by_description = [item for item in data if item.get('description') in classification]
+    descriptions = [item['description'] for item in sorted_by_description]
+    statistics_dict = Counter(descriptions)
+
+    return dict(statistics_dict)
